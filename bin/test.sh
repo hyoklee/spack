b8100f2cf8d7a917546e4a3652d2061094a9991c
H. Joe Lee <hyoklee@hdfgroup.org>
2020-12-17 15:46:26 -0600

    OESS-104:Build SZ filter.

--- bin/test.sh
+++ bin/test.sh
@@ -1,4 +1,9 @@
+cd /scr/hyoklee/src/
+tar cvf hdf5_plugins.tar hdf5_plugins/*
+gzip hdf5_plugins.tar
+mv hdf5_plugins.tar.gz ../x/
+cd spack-hyoklee/bin/
 rm -rf ~/.spack/cache
 rm -rf /tmp/hyoklee
-./spack uninstall --all hdf5-cmake
+./spack uninstall --all --force hdf5-cmake
 ./spack install hdf5-cmake
