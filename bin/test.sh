c3c3e8cf9b2559caa8799872cfc45e29963586a7
H. Joe Lee <hyoklee@hdfgroup.org>
2020-12-23 09:42:35 -0600

    OESS-104:Unset HDF5_PLUGIN_PATH explicitly to check the effect of the environment variable.

--- bin/test.sh
+++ bin/test.sh
@@ -2,18 +2,19 @@
 rm -rf ~/.spack/cache
 rm -rf /tmp/hyoklee
 ./spack uninstall --all --force --yes-to-all hdf5-cmake
-./spack install hdf5-cmake~lzf~lz4
+./spack install hdf5-cmake
 source ../share/spack/setup-env.csh
 spack load hdf5-cmake
 
+unsetenv HDF5_PLUGIN_PATH
 # This will fail.
 h5dump-shared /scr/hyoklee/data/test_zfp_be.h5
 
-# Change install path.
+# Set filter plugin environment variable.
 set p="`./spack find --paths hdf5-cmake | tail  -1 | cut -d' ' -f 3-`"
 setenv HDF5_PLUGIN_PATH $p/lib/plugin/
 
-# These test will succeed.
+# These tests will succeed.
 h5dump-shared /scr/hyoklee/data/test_zfp_be.h5
 h5dump-shared /scr/hyoklee/data/test_zfp_le.h5
 h5dump-shared /scr/hyoklee/data/test_sz.h5
