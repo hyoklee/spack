13217f78a101a3e7832845518be0a03cbdff9818
H. Joe Lee <hyoklee@hdfgroup.org>
2020-12-22 22:35:39 -0600

    OESS-104:Change default option to enable all filters. Turn off lzf and lz4 on-demand.

--- bin/test.sh
+++ bin/test.sh
@@ -7,7 +7,7 @@
 rm -rf ~/.spack/cache
 rm -rf /tmp/hyoklee
 ./spack uninstall --all --force --yes-to-all hdf5-cmake
-./spack install hdf5-cmake
+./spack install hdf5-cmake~lzf~lz4
 source ../share/spack/setup-env.csh
 # For Bash
 # . ../share/spack/setup-env.sh
@@ -15,7 +15,7 @@ spack load hdf5-cmake
 # This will fail.
 h5dump-shared /scr/hyoklee/data/test_zfp_be.h5
 # Change install path.
-setenv HDF5_PLUGIN_PATH /scr/hyoklee/src/spack-hyoklee/opt/spack/linux-centos7-haswell/gcc-4.8.5/hdf5-cmake-develop-7trbcmpjnklt72hoqxzswf7r3tezj7sz/lib/plugin/
+setenv HDF5_PLUGIN_PATH /scr/hyoklee/src/spack-hyoklee/opt/spack/linux-centos7-haswell/gcc-4.8.5/hdf5-cmake-develop-uqtei4sbgqc33w5gmi6zfxmajxvyckm6/lib/plugin/
 
 # These test will succeed.
 h5dump-shared /scr/hyoklee/data/test_zfp_be.h5
