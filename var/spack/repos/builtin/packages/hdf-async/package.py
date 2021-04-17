78ebb42d881e1cec25503dd6646237936dae1a90
H. Joe Lee <hyoklee@hdfgroup.org>
2021-04-17 00:08:16 -0500

    OESS-147:Disable setup environment.

--- var/spack/repos/builtin/packages/hdf-async/package.py
+++ var/spack/repos/builtin/packages/hdf-async/package.py
@@ -23,19 +23,37 @@
 from spack import *
 
 
-class HdfAsync(Package):
+class HdfAsync(CMakePackage):
     """FIXME: Put a proper description of your package here."""
 
     homepage = "https://sdm.lbl.gov/"
-    git      = "https://github.com/hpc-io/vol-async"
+    # git      = "https://github.com/hpc-io/vol-async"
+    git      = "https://github.com/hyoklee/vol-async"
     maintainers = ['hyoklee']
 
     version('default', branch='async_vol_register_optional', preferred=True)
 
     # FIXME: Add dependencies if required.
     depends_on('hdf5-av')
-    patch('Makefile.patch')
-    patch('src_Makefile.patch')
-    def install(self, spec, prefix):
-        make()
-        make('install')
+    # patch('Makefile.patch')
+    # patch('src_Makefile.patch')
+    # patch('test_Makefile.patch')
+    def cmake_args(self):
+        """Populate cmake arguments for HDF5 DAOS."""
+        spec = self.spec
+
+        args = [
+            '-DBUILD_SHARED_LIBS:BOOL=ON',
+            '-DBUILD_TESTING:BOOL=ON'
+        ]
+
+        return args
+    
+
+    #def setup_run_environment(self, env):
+        # env.prepend_path('HDF5_PLUGIN_PATH', self.prefix.lib)
+        #env.prepend_path('HDF5_VOL_CONNECTOR',
+        #                 'async under_vol=0;under_info={}')        
+
+    def check(self):
+        make('test')
