dbfd4458c59a581ba8e0e38ba6577751da9c6526
H. Joe Lee <hyoklee@hdfgroup.org>
2021-04-19 15:19:31 -0500

    OESS-147:Add vol-async test script.

---
+++ bin/test_va.sh
@@ -0,0 +1,26 @@
+#!/bin/tcsh
+# ./spack uninstall --all --force --yes-to-all hdf5-av
+# rm -rf ~/.spack/cache
+# rm -rf /tmp/hyoklee
+
+./spack install hdf-async
+
+source ../share/spack/setup-env.csh
+spack load hdf-async
+
+# Set filter plugin environment variable.
+set p="`./spack find --paths hdf-async | tail  -1 | cut -d' ' -f 3-`"
+setenv HDF5_PLUGIN_PATH $p/lib/
+
+# Show installed plugin.
+ls $p/lib/
+
+# Show installed header.
+ls $p/include/
+
+
+
+
+
+
+
