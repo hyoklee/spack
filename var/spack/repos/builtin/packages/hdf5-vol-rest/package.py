60dc50df37929a7f732e5f170541ebab923b0db5
Hyo-Kyung Lee <hyoklee@hdfgroup.org>
2021-07-22 19:23:39 -0500

    OESS-166:Handle submodules.

--- var/spack/repos/builtin/packages/hdf5-vol-rest/package.py
+++ var/spack/repos/builtin/packages/hdf5-vol-rest/package.py
@@ -16,27 +16,8 @@ HDF5 API calls into HTTP-based REST calls, as defined by the HDF5 REST API."""
     maintainers = ['hyoklee']
 
     version('develop', branch='hdf5_1_12_update', 
-            git='https://github.com/HDFGroup/vol-rest.git', preferred=True)
+            git='https://github.com/HDFGroup/vol-rest.git', preferred=True,
+            submodules=True)
 
-    depends_on('cmake@3.19.7')
     depends_on('yajl')
-
-    def cmake_args(self):
-        """Set CMake arguments."""
-        spec = self.spec
-
-        args = [
-            '-DBUILD_SHARED_LIBS:BOOL=ON',
-            '-DBUILD_TESTING:BOOL=ON'
-        ]
-
-        return args
     
-
-    #def setup_run_environment(self, env):
-        # env.prepend_path('HDF5_PLUGIN_PATH', self.prefix.lib)
-        # env.prepend_path('HDF5_VOL_CONNECTOR',
-        #                 'async under_vol=0;under_info={}')        
-
-    def check(self):
-        make('test')
