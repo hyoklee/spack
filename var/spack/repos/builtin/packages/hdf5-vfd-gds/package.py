1903ed1a34ec24ebbe9e70b00ebef543bb2bf7a8
Hyo-Kyung Lee <hyoklee@hdfgroup.org>
2021-11-17 23:45:34 -0600

    OESS-155: Create hdf5-vfd-gds package.

---
+++ var/spack/repos/builtin/packages/hdf5-vfd-gds/package.py
@@ -0,0 +1,35 @@
+# Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class Hdf5VfdGds(CMakePackage, CudaPackage):
+    """This pacakge enables GPU Direct Storage Virtual File Driver in HDF5."""
+
+    # Package info
+    homepage    = 'https://github.com/hpc-io/vfd-gds'
+    # url         = 'https://github.com/LLNL/zfp/releases/download/0.5.5/zfp-0.5.5.tar.gz'
+    git         = 'https://github.com/hpc-io/vfd-gds.git'
+    maintainers = ['hyoklee']
+
+    # Versions
+    version('1.0.0', branch='master', default=True)
+
+    # Dependencies
+    depends_on('cmake@3.4.0:')
+    depends_on('hdf5@develop')    
+    depends_on('cuda')
+
+    def cmake_args(self):
+        spec = self.spec
+
+        # CMake options
+        args = [
+            self.define('BUILD_TESTING', self.run_tests),
+            self.define()
+        ]
+
+        return args
