cd3ef839db68a822bc69bb1094798e8d27684cae
Hyo-Kyung Lee <hyoklee@hdfgroup.org>
2021-11-18 23:01:58 -0600

    OESS-155: Add the official release version and sha256 sum.

--- var/spack/repos/builtin/packages/hdf5-vfd-gds/package.py
+++ var/spack/repos/builtin/packages/hdf5-vfd-gds/package.py
@@ -5,24 +5,24 @@
 
 from spack import *
 
-
 class Hdf5VfdGds(CMakePackage, CudaPackage):
     """This pacakge enables GPU Direct Storage Virtual File Driver in HDF5."""
 
     # Package info
     homepage    = 'https://github.com/hpc-io/vfd-gds'
-    # url         = 'https://github.com/LLNL/zfp/releases/download/0.5.5/zfp-0.5.5.tar.gz'
+    url = 'https://github.com/hpc-io/vfd-gds/archive/refs/tags/1.0.0.tar.gz'
     git         = 'https://github.com/hpc-io/vfd-gds.git'
-    maintainers = ['hyoklee']
+    maintainers = ['hyoklee', 'lrknox']
 
     # Versions
-    version('1.0.0', branch='master')
-    version('hyoklee', branch='master', git='https://github.com/hyoklee/vfd-gds.git', default=True)
+    version('master', branch='master')
+    version('1.0.0', sha256='6b16105c7c49f13fc05784ee69b78d45fb159270c78d760689f9cd21e230ddd2', default=True)
     
     # Dependencies
-    depends_on('cmake@3.4.0:')
+    depends_on('cuda')    
+    depends_on('cmake@3.12:')
     depends_on('hdf5@develop-1.13')    
-    depends_on('cuda')
+
 
     def cmake_args(self):
         spec = self.spec
