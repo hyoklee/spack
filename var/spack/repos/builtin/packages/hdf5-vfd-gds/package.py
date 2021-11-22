10b0f0772b96757906bc7c312900cb553727bdbe
H. Joe Lee <hyoklee@hdfgroup.org>
2021-11-21 22:20:22 -0600

    Update package.py

--- var/spack/repos/builtin/packages/hdf5-vfd-gds/package.py
+++ var/spack/repos/builtin/packages/hdf5-vfd-gds/package.py
@@ -10,13 +10,14 @@ class Hdf5VfdGds(CMakePackage, CudaPackage):
 
     # Package info
     homepage    = 'https://github.com/hpc-io/vfd-gds'
-    url = 'https://github.com/hpc-io/vfd-gds/archive/refs/tags/1.0.0.tar.gz'
-    git         = 'https://github.com/hpc-io/vfd-gds.git'
+    # url = 'https://github.com/hpc-io/vfd-gds/archive/refs/tags/1.0.0.tar.gz'
+    # git         = 'https://github.com/hpc-io/vfd-gds.git'
+    git         = 'https://github.com/hyoklee/vfd-gds.git'
     maintainers = ['hyoklee', 'lrknox']
 
     # Versions
-    version('master', branch='master')
-    version('1.0.0', sha256='6b16105c7c49f13fc05784ee69b78d45fb159270c78d760689f9cd21e230ddd2', default=True)
+    version('master', branch='master', default=True)
+    # version('1.0.0', sha256='6b16105c7c49f13fc05784ee69b78d45fb159270c78d760689f9cd21e230ddd2', default=True)
     
     # Dependencies
     depends_on('cuda')    
