91ab1bb53edc7b3b01380159f7b63ce665e3eb79
Hyo-Kyung Lee <hyoklee@hdfgroup.org>
2022-01-05 17:18:27 -0600

    Update release version for hdf5-vfd-gds.

--- var/spack/repos/builtin/packages/hdf5-vfd-gds/package.py
+++ var/spack/repos/builtin/packages/hdf5-vfd-gds/package.py
@@ -10,20 +10,19 @@ class Hdf5VfdGds(CMakePackage, CudaPackage):
 
     # Package info
     homepage    = 'https://github.com/hpc-io/vfd-gds'
-    # url = 'https://github.com/hpc-io/vfd-gds/archive/refs/tags/1.0.0.tar.gz'
-    # git         = 'https://github.com/hpc-io/vfd-gds.git'
-    git         = 'https://github.com/hyoklee/vfd-gds.git'
+    url = 'https://github.com/hpc-io/vfd-gds/archive/refs/tags/1.0.1.tar.gz'
+    git         = 'https://github.com/hpc-io/vfd-gds.git'
     maintainers = ['hyoklee', 'lrknox']
 
     # Versions
-    version('master', branch='master', default=True)
-    # version('1.0.0', sha256='6b16105c7c49f13fc05784ee69b78d45fb159270c78d760689f9cd21e230ddd2', default=True)
+    version('master', branch='master')
+    version('1.0.1', sha256='00e125fd149561be991f41e883824de826d8add604aebccf103a4fb82d5faac2', default=True)
+    version('1.0.0', sha256='6b16105c7c49f13fc05784ee69b78d45fb159270c78d760689f9cd21e230ddd2')
     
     # Dependencies
-    depends_on('libpthread-stubs')
     depends_on('cuda')    
     depends_on('cmake@3.12:')
-    depends_on('hdf5@develop-1.13')    
+    depends_on('hdf5@1.13.0:')    
 
 
     def cmake_args(self):
@@ -32,7 +31,6 @@ class Hdf5VfdGds(CMakePackage, CudaPackage):
         # CMake options
         args = [
             self.define('BUILD_TESTING', self.run_tests),
-            # self.define('CUDA_TOOLKIT_ROOT_DIR', '/usr/local/cuda-11.5/'),            
         ]
 
         return args
