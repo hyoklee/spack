08fdfc09fe767fdb2145ad50012b602371b2c0c0
H. Joe Lee <hyoklee@hdfgroup.org>
2022-07-12 14:25:53 -0500

    Check style.

--- var/spack/repos/builtin/packages/hermes/package.py
+++ var/spack/repos/builtin/packages/hermes/package.py
@@ -2,9 +2,7 @@
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
-
 from spack.package import *
-
 
 
 class Hermes(CMakePackage):
@@ -19,12 +17,12 @@ class Hermes(CMakePackage):
     maintainers = ['hyoklee']
 
     version('master', branch='master')
-    version('0.4.0-beta', url="https://github.com/HDFGroup/hermes/archive/v0.4.0-beta.tar.gz", sha256='e8bd0b701149844f7f113a55a107e40b3579945d70ca2cb8ce9db5b5a131efdb')
+    version('0.7.0-beta', url="https://github.com/HDFGroup/hermes/archive/v0.7.0-beta.tar.gz", sha256='1046f537558e479c8a828fe8e289da410a0f82bdba199a40ea7ff0eb842d9382')
 
     variant('vfd', default=False, description='Enable HDF5 VFD')
 
     depends_on('mochi-thallium~cereal@0.8:')
-    depends_on('catch2@2.13:')
+    depends_on('catch2')
     depends_on('glpk')
     depends_on('glog@0.4.0:')
     depends_on('mpi')
