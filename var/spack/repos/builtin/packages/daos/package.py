d348ee6b4e26e20806f3b9771aae8cfae2ac4e3b
Hyo-Kyung Lee <hyoklee@hdfgroup.org>
2023-03-07 08:12:36 -0600

    ci: add dpdk variant to spdk

--- var/spack/repos/builtin/packages/daos/package.py
+++ var/spack/repos/builtin/packages/daos/package.py
@@ -41,7 +41,7 @@ class Daos(SConsPackage):
     depends_on("py-distro")
     depends_on("readline")
     depends_on("scons@4.4.0:")
-    depends_on("spdk+shared+rdma")
+    depends_on("spdk+shared+rdma+dpdk")
     depends_on("ucx")
 
     def build_args(self, spec, prefix):
