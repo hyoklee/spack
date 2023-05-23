16f9ba8c9d93bff16925e21af9bf7a0f8dbbe5f2
Hyo-Kyung Lee <hyoklee@hdfgroup.org>
2023-05-22 21:37:22 -0500

    ci: test h5py with hdf5 develop

--- var/spack/repos/builtin/packages/py-h5py/package.py
+++ var/spack/repos/builtin/packages/py-h5py/package.py
@@ -62,10 +62,12 @@ class PyH5py(PythonPackage):
 
     # Link dependencies (py-h5py v2 cannot build against HDF5 1.12 regardless
     # of API setting)
-    depends_on("hdf5@1.8.4:1.11 +hl", when="@:2")
-    depends_on("hdf5@1.8.4:1.12 +hl", when="@3:3.7")
-    depends_on("hdf5@1.8.4:1.14 +hl", when="@3.8:")
+    depends_on("hdf5@develop-1.15", when="@master")
+    # depends_on("hdf5@1.8.4:1.11 +hl", when="@:2")
+    # depends_on("hdf5@1.8.4:1.12 +hl", when="@3:3.7")
+    # depends_on("hdf5@1.8.4:1.14 +hl", when="@3.8:")
 
+        
     # MPI dependencies
     depends_on("hdf5+mpi", when="+mpi")
     depends_on("mpi", when="+mpi")
